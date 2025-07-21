3d6515ba47a40ea339b97f9169154a4f37391d02
kasindala-abhilasha <22211a3226@bvrit.ac.in>
2025-07-21 16:29:40 +0530

    Fix: Remove basename for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/sweetspot-react"> {/* 👈 Only if hosted in subpath like GitHub Pages */}
+    <BrowserRouter> {/* ✅ Remove basename */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
